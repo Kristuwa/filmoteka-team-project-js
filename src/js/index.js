76749d793d4061a4dd6a5e811b8998fb9a9b008f
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2022-10-29 02:04:37 +0300

    add modal film

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,7 @@ import './userAuth/firebase-auth-import';
 import { renderMarkupTrending } from './render_trending.js';
 
 renderMarkupTrending();
+
+// document.addEventListener('DOMContentLoaded', onModalOpenFilm);
+import { onModalOpenFilm } from './modal-film';
+import { onModalFilmClose } from './modal-film';
---
+++ src/js/modal-film.js
@@ -0,0 +1,124 @@
+import { QueryHandler } from './query_handler';
+import genresJson from './genres.json';
+
+const modalFilmClick = document.querySelector('.card-list');
+const modalBackdrop = document.querySelector('.modalbackdrop-film');
+const modalFilm = document.querySelector('.modal-film');
+let modalBackdropActive;
+console.log(modalBackdropActive);
+// const modalFilmBtnClose = document.querySelector('.film-card-close');
+const newFetch = new QueryHandler();
+
+console.log(modalFilmClick);
+
+modalFilmClick.addEventListener('click', onModalOpenFilm);
+
+export function onModalOpenFilm(e) {
+  e.preventDefault();
+
+  modalBackdrop.classList.add('active');
+  modalBackdropActive = document.querySelector('.modalbackdrop-film.active');
+
+  modalBackdropActive.addEventListener('click', onModalFilmClose);
+  modalFilm.classList.add('active');
+  document.body.classList.add('is-hidden');
+
+  const id = Number(e.target.closest('li').dataset.id);
+  newFetch.movieId = id;
+  console.log(id);
+  return newFetch.fetchQueryResultsForMovieInfo().then(data => {
+    console.log(data);
+
+    const markup = createMarkupModal(data);
+
+    //  closeBtn.addEventListener('click', onModalFilmClose);
+    return (modalFilm.innerHTML = markup);
+  });
+}
+
+export function onModalFilmClose(e) {
+  //   const closeBtn = e.target.closest('.film-card-close');
+  //   console.log(closeBtn);
+  if (e.target.closest('.film-card-close')) {
+    modalBackdrop.classList.remove('active');
+    modalFilm.classList.remove('active');
+    document.body.classList.remove('is-hidden');
+  }
+}
+
+function createMarkupModal({
+  title,
+  backdrop_path,
+  vote_average,
+  vote_count,
+  overview,
+  popularity,
+  original_title,
+  genres,
+}) {
+  const IMAGE_URL = 'https://image.tmdb.org/t/p/w500/';
+  let genresString;
+  const genresArray = [];
+  console.log(genres);
+  const selectedGenres = genres.map(id => {
+    return genresJson.filter(idGenre => idGenre.id === id);
+  });
+  selectedGenres.map(genre => genresArray.push(genre.name));
+  if (genresArray.length > 0 && genresArray.length <= 3) {
+    genresString = genresArray.join(', ');
+  } else {
+    genresString = `${genresArray[0]}, ${genresArray[1]}, other`;
+  }
+
+  return `<div class="film-card">
+	<button type="button" class="film-card-close" autofocus>
+	  x<svg class="film-card-close-svg" width="14" height="14">
+		 <use href="./images/icons.svg#icon-close"></use>
+	  </svg>
+	</button>
+	<img src="${IMAGE_URL}${backdrop_path}" alt="film-poster" />
+	<table>
+	  <tbody>
+		 <tr>
+			<th class='film-card__title'>${title}</th>
+		 </tr>
+		 <tr>
+			<td>Vote / Votes</td>
+			<td>${vote_average} / ${vote_count}</td>
+		 </tr>
+		 <tr>
+			<td>Popularity</td>
+			<td>${popularity}</td>
+		 </tr>
+		 <tr>
+			<td>Original Title</td>
+			<td>${original_title}</td>
+		 </tr>
+		 <tr>
+			<td>Genre</td>
+			<td>${genresString}</td>
+		 </tr>
+		 <tr>
+			<td>About</td>
+		 </tr>
+		 <tr>
+			<td class='film-card__text'>
+			  ${overview}
+			</td>
+		 </tr>
+		 <tr>
+			<td>
+			  <button type="button" class="film-card-addToWatched">
+				 Add to watched
+			  </button>
+			</td>
+			<td>
+			  <button type="button" class="film-card-addToQueue">
+				 Add to queue
+			  </button>
+			</td>
+		 </tr>
+	  </tbody>
+	</table>
+	</div>`;
+}
