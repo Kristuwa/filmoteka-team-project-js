897fbe2171cf712168b98295eba8b59a50aba6e3
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2022-10-29 02:45:27 +0300

    Update acive_button.js

--- src/js/acive_button.js
+++ src/js/acive_button.js
@@ -1,22 +1,22 @@
 // ✅ Заміна класів is-active
-const libraryListButtons = document.querySelector('.btn__list')
-libraryListButtons.addEventListener('click', onButtonClick);
+const libraryListButtons = document.querySelector('.btn__list');
+// libraryListButtons.addEventListener('click', onButtonClick);
 
 export function onButtonClick(event) {
- if (event.target.nodeName !== 'BUTTON') {
-  return;
- }
- // console.log(event.target);
+  if (event.target.nodeName !== 'BUTTON') {
+    return;
+  }
+  // console.log(event.target);
 
-const currentActiveBtn = libraryListButtons.querySelector('.is-active')
- // console.log(currentActiveBtn);
+  const currentActiveBtn = libraryListButtons.querySelector('.is-active');
+  // console.log(currentActiveBtn);
 
- if (currentActiveBtn) {
-   currentActiveBtn.classList.remove('is-active')
- // console.log('remove class is-active');
- }
+  if (currentActiveBtn) {
+    currentActiveBtn.classList.remove('is-active');
+    // console.log('remove class is-active');
+  }
 
- // додає клас на кнопку на яку клікнули
- event.target.classList.add('is-active')
- // console.log('add class is-active');
+  // додає клас на кнопку на яку клікнули
+  event.target.classList.add('is-active');
+  // console.log('add class is-active');
 }
