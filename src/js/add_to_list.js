6fa1840e8946394e101f269908c49c99bf3b9224
Alex-Sorokolit <sorokolital@gmail.com>
2022-10-28 23:05:51 +0300

    up

--- src/js/add_to_list.js
+++ src/js/add_to_list.js
@@ -1,7 +1,9 @@
 // Імпорти _________________________________________________________
-// import storage from "./locale-storage-methods";
-// import QueryHandler from "./query_handler";
+import storage from "./locale-storage-methods";
+import QueryHandler from "./query_handler";
+import { createCardMarkup } from './card_markup';
 
 // const query = new QueryHandler();
 
-// console.log(query);
+
+// вибір фільму
