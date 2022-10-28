6efe46aa133964b44195ccdab46cbb0d991d0f48
Alex-Sorokolit <sorokolital@gmail.com>
2022-10-28 22:39:26 +0300

    start

---
+++ src/js/add_to_list.js
@@ -0,0 +1,7 @@
+// Імпорти _________________________________________________________
+// import storage from "./locale-storage-methods";
+// import QueryHandler from "./query_handler";
+
+// const query = new QueryHandler();
+
+// console.log(query);
