dd26c6289c982e860fadb57db3bad47e5906a924
Alex-Sorokolit <sorokolital@gmail.com>
2022-10-28 23:20:38 +0300

    active button

--- src/js/add_to_list.js
+++
@@ -1,9 +0,0 @@
-// Імпорти _________________________________________________________
-import storage from "./locale-storage-methods";
-import QueryHandler from "./query_handler";
-import { createCardMarkup } from './card_markup';
-
-// const query = new QueryHandler();
-
-
-// вибір фільму
--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,4 @@ import './userAuth/firebase-auth-import';
 import { renderMarkupTrending } from './render_trending.js';
 
 renderMarkupTrending();
+ import './acive_button'
