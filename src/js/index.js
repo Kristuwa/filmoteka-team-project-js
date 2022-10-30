41ac509c20b86be440d3a0693a23978ca8f912c9
ViacheslavMiakota <parazyt2010@gmail.com>
2022-10-30 12:34:25 +0200

    Add body
    Додав фон на боді.
    Стилі мадалкі
    прижав футер до низу

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import './userAuth/firebase-auth-import';
 
 import { renderMarkupTrending } from './render_trending.js';
-
+import { toggleCard, toggleModal } from './modal-team.js';
 renderMarkupTrending();
+toggleModal();
+toggleCard();
