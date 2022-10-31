3b110381508721ba5bc2a72c388059020721763f
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2022-10-31 16:51:41 +0200

    Update page-library.js

--- src/js/page-library.js
+++ src/js/page-library.js
@@ -1,7 +1,10 @@
 import './userAuth/firebase-auth-import';
 import Spinner from './spinner';
+import './acive_button';
 import './add_to_list';
 import './library_lists';
+
+
 import { onModalOpenFilm } from './modal-film';
 import { onBackdropClick } from './modal-film';
 import { onEscBtnPress } from './modal-film';
