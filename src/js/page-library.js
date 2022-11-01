ae56c84d19203cbd7ce7ffcd78d46b6f58f7557c
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2022-11-01 14:44:12 +0200

    add trailer on page library

--- src/js/page-library.js
+++ src/js/page-library.js
@@ -1,6 +1,6 @@
 import './userAuth/firebase-auth-import';
-import './active_button';
 import Spinner from './spinner';
+import './active_button';
 import { saveGenres } from './genres_storage';
 import './add_to_list';
 import './library_lists';
@@ -10,10 +10,11 @@ import './day-night-library';
 import './userAuth/firebase-library';
 saveGenres();
 import { onWachedLibBtnClick } from './library_lists';
-import { refs } from "./refs";
+import { refs } from './refs';
 import { onChangePageClick } from './render_trending';
+import { onPlayVideo } from './play_trailer_btn';
+import { openModalTrailer, closeModalTrailer } from './play_trailer_btn';
 
 refs.pagination.removeEventListener('click', onChangePageClick);
 
 onWachedLibBtnClick();
-
